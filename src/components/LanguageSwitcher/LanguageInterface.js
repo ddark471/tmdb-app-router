9a71251acaf6d17b872d320d37406e8bc169692f
ddark471 <ddarkk471@gmail.com>
2023-09-09 19:32:08 +0500

    feat(LanguageSwitcher): implemented eslint

--- src/components/LanguageSwitcher/LanguageInterface.js
+++ src/components/LanguageSwitcher/LanguageInterface.js
@@ -1,38 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { useTranslation } from "react-i18next";
 import styles from "./LanguageSwitcher.module.css";
 
-const LanguageInterface = ({
-  handleOnMouseEnter,
-  handleOnMouseLeave,
-  handleClick,
-  isActive,
-}) => {
+const LanguageInterface = ({ handleOnMouseEnter, handleOnMouseLeave, handleClick, isActive }) => {
   const { i18n } = useTranslation();
 
   return (
     <div className={styles.languageContainer}>
       <div
-        className={`${styles.languageSwitcher} ${
-          isActive ? styles.switchModifier : ""
-        }`}
+        className={`${styles.languageSwitcher} ${isActive ? styles.switchModifier : ""}`}
         onMouseEnter={handleOnMouseEnter}
         onMouseLeave={handleOnMouseLeave}
       >
-        <span className={styles.currentLang}>
-          {i18n.language.toUpperCase()}
-        </span>
+        <span className={styles.currentLang}>{i18n.language.toUpperCase()}</span>
         {isActive && (
           <div className={styles.langDropdown}>
             <div
               className={styles.language}
-              onClick={(event) =>
-                handleClick(event, i18n.language == "en" ? "ru" : "en")
-              }
+              onClick={(event) => handleClick(event, i18n.language == "en" ? "ru" : "en")}
             >
-              <span className={styles.languageText}>
-                {i18n.language == "en" ? "RU" : "EN"}
-              </span>
+              <span className={styles.languageText}>{i18n.language == "en" ? "RU" : "EN"}</span>
             </div>
           </div>
         )}
