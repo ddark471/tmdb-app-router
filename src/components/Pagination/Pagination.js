9f981d7bfb1620dc405a5075908fee0f5b029517
ddark471 <ddarkk471@gmail.com>
2023-09-09 19:38:51 +0500

    feat(Pagination): implemented eslint

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,29 +1,25 @@
 import React from "react";
-import { Link, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import styles from "./Pagination.module.css";
 
 const Pagination = ({ handleNextPage, handleBackPage, searchResults }) => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const page = parseInt(searchParams.get("page", 10));
 
   return (
     <div className={styles.pagination}>
-      <button
-        className={styles.pagesButton}
-        onClick={handleBackPage}
-        disabled={page === 1}
-      >
+      <button className={styles.pagesButton} onClick={handleBackPage} disabled={page === 1}>
         <div className={styles.buttonImage}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-chevron-left"
+            className="bi bi-chevron-left"
             viewBox="0 0 16 16"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
             />
           </svg>
@@ -51,9 +47,7 @@ const Pagination = ({ handleNextPage, handleBackPage, searchResults }) => {
       <button
         className={styles.pagesButton}
         onClick={handleNextPage}
-        disabled={
-          page >= 500 || searchResults ? searchResults.length < 20 : null
-        }
+        disabled={page >= 500 || searchResults ? searchResults.length < 20 : null}
       >
         <div className={styles.buttonImage}>
           <svg
@@ -61,11 +55,11 @@ const Pagination = ({ handleNextPage, handleBackPage, searchResults }) => {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-chevron-right"
+            className="bi bi-chevron-right"
             viewBox="0 0 16 16"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
             />
           </svg>
