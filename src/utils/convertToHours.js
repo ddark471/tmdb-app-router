1988f191ef76272a383ca3f8c475d6601668e42e
ddark471 <ddarkk471@gmail.com>
2023-09-09 20:24:14 +0500

    feat(convertToHours.js): implemented eslint

--- src/utils/convertToHours.js
+++ src/utils/convertToHours.js
@@ -1,13 +1,11 @@
 export const convertToHours = (minutes) => {
   const getMinutes = minutes % 60;
   const getHours = Math.floor(minutes / 60);
-  let total;
-  if (getMinutes === 0) return (total = getHours);
-  else
-    return (total = {
+  if (getMinutes === 0) return getHours;
+  else {
+    return {
       hours: getHours,
       minutes: getMinutes,
-    });
-
-  return total;
+    };
+  }
 };
