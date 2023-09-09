b3dba58abd57358e000070327d76eb23913e0b50
ddark471 <ddarkk471@gmail.com>
2023-09-09 19:26:52 +0500

    feat(ActorImages): implemented eslint

--- src/components/ActorImages/ActorImages.js
+++ src/components/ActorImages/ActorImages.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { service } from "api/service";
 import ModalImage from "react-modal-image";
 import styles from "./ActorImages.module.css";
 
 const ActorImages = ({ person_id }) => {
-  const { data, isError, isSuccess, error } = useQuery({
+  const { data, isSuccess } = useQuery({
     queryKey: ["ActorImages", person_id],
     queryFn: async () => {
       const { data } = await service(`/person/${person_id}/images`, "GET");
