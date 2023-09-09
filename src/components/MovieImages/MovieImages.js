b1d37966b18bdae5ce073db888b4d186566f91f0
ddark471 <ddarkk471@gmail.com>
2023-09-09 19:34:10 +0500

    feat(MovieImages): implemented eslint

--- src/components/MovieImages/MovieImages.js
+++ src/components/MovieImages/MovieImages.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { service } from "api/service";
 import ModalImage from "react-modal-image";
